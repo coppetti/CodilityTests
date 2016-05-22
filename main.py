8f8daf9f814a88a186699bcf7e1d36bc3191e2c2
Matheus Coppetti <matheuscoppetti@gmail.com>
2016-05-22 15:51:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def cyclicRotation(A, K):
 # 2.2 OddsOccurence
 def oddOccur(A):
    return [a for a in A if A.count(a)%2==1][0]
+   
+# 3.1 FrogJmp
+def frogJmp(X, Y, D):
+    return [(Y-X)/D if (Y-X)%D==0 else ((Y-X)/D)+1][0]
     
 def main():
   binaryGap(15)
   cyclicRotation([3, 8, 9, 7, 6], 3) 
   oddsOccur([9, 3, 9, 3, 9, 7, 9])
+  frogJmp(10,85,30)
+
