424dc13e59ebc9ad7bb1c004b49a6ce691731ad4
Matheus Coppetti <matheuscoppetti@gmail.com>
2016-05-22 16:23:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 # 1.1 BinaryGap
 def binaryGap(N):
     return len(max(str("{0:b}".format(N)).split("1")))
+   
     
 # 2.1 CyclicRotation
 def cyclicRotation(A, K):
     return A[-K:]+A[:-K]
 
+
 # 2.2 OddsOccurence
 def oddOccur(A):
    return [a for a in A if A.count(a)%2==1][0]
    
+   
 # 3.1 FrogJmp
 def frogJmp(X, Y, D):
     return [(Y-X)/D if (Y-X)%D==0 else ((Y-X)/D)+1][0]
+
 
 # 3.2 TapeEquilibrium
 def tapeEq(A):def solution(A):
@@ -20,15 +24,17 @@ def tapeEq(A):def solution(A):
     for i in range(len(A)):
         min = abs(sum(A[i:])-sum(A[:i])) if abs(sum(A[i:])-sum(A[:i]))<min else min
     return min
+    
+    
 # 3.3 PermMissingElem
 def permMissingElem(A):
     A.sort()
     return [A[i]-1 for i in range(len(A)) if A[i]<>i+1][0]
     
-def main():
-  print "BinaryGap: ",binaryGap(15)
-  print "CyclicRotation: ",cyclicRotation([3, 8, 9, 7, 6], 3) 
-  print "OddsOccurence: ",oddsOccur([9, 3, 9, 3, 9, 7, 9])
-  print "FrogJmp: ",frogJmp(10,85,30)
-  print "TapeEquilibrium: ",tapeEq([3, 1, 2, 4, 3])
-  print "PermMissingElem: ",permMissingElem([2, 1, 4, 5])
+
+print "BinaryGap: ",binaryGap(15)
+print "CyclicRotation: ",cyclicRotation([3, 8, 9, 7, 6], 3) 
+print "OddsOccurence: ",oddsOccur([9, 3, 9, 3, 9, 7, 9])
+print "FrogJmp: ",frogJmp(10,85,30)
+print "TapeEquilibrium: ",tapeEq([3, 1, 2, 4, 3])
+print "PermMissingElem: ",permMissingElem([2, 1, 4, 5])
