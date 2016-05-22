6ca686eaedbb74cd31f1d697e047a53458d582e5
Matheus Coppetti <matheuscoppetti@gmail.com>
2016-05-22 15:59:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ def oddOccur(A):
 # 3.1 FrogJmp
 def frogJmp(X, Y, D):
     return [(Y-X)/D if (Y-X)%D==0 else ((Y-X)/D)+1][0]
+
+# 3.2 TapeEquilibrium
+def tapeEq(A):def solution(A):
+    min = max(A)
+    for i in range(len(A)):
+        min = abs(sum(A[i:])-sum(A[:i])) if abs(sum(A[i:])-sum(A[:i]))<min else min
+    return min
     
 def main():
   binaryGap(15)
   cyclicRotation([3, 8, 9, 7, 6], 3) 
   oddsOccur([9, 3, 9, 3, 9, 7, 9])
   frogJmp(10,85,30)
-
+  tapeEq([3, 1, 2, 4, 3])
